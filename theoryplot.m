96ea5337ffed084c9f98d3ccf354beb6bac19e6d
EngScientist <engscientist.me@gmail.com>
2020-05-01 18:55:11 +0800

    Final update

--- theoryplot.m
+++ theoryplot.m
@@ -1,5 +1,6 @@
+close all
 %% CONCENTRATION OF SODIUM CHLORIDE IN SOLUTION
-c = [135; 140; 145; 150] / 1000;
+c = [130:155] / 1000;
 
 %% RELATIVE PERMITTIVITY MODEL
 modelepsilonwater = 78.3;
@@ -20,7 +21,7 @@ mediumz = 2.3 / 1000;
 epsilonvacuum = (1 / (36 * pi)) * (10 ^ (-9));
 muvacuum = 4 * pi * (10 ^ (-7));
 % VECTORS
-f = (1350:2700) * (10 ^ 6);
+f = 2185 * (10 ^ 6);
 angfreq = 2 * pi * f;
 PARTA1 = sqrt(murelative * muvacuum ./ (modelepsilonrelative .* epsilonvacuum));
 PARTA2 = (1 + ((modelsigma ./ (angfreq .* modelepsilonrelative .* epsilonvacuum)) .^ 2)) .^ (-1 / 4);
@@ -31,5 +32,16 @@ PARTB1 = sqrt(PARTB11 .* PARTB12);
 PARTB = exp(-2 .* angfreq .* mediumz .* PARTB1);
 PARTC1 = modelsigma ./ (angfreq .* modelepsilonrelative .* epsilonvacuum);
 PARTC = cos((1 / 2) .* atan(PARTC1));
+x = c;
 y = PARTA .* PARTB .* PARTC;
-plot(f,y,'LineWidth',1.25)
+plot(x,y,'LineWidth',1.25)
+
+xlabeltext = '[NaCl] (in mol/L)';
+ylabeltext = '$S(f:c,f_{0})$';
+xlab = xlabel(xlabeltext,'interpreter','latex');
+ylab = ylabel(ylabeltext,'interpreter','latex');
+tit = title('Plot of $S(f:c,f_{0})$ against [NaCl] (in mol/L)','interpreter','latex');
+set(tit,'FontSize',14)
+set(xlab,'FontSize',14)
+set(ylab,'FontSize',14)
+set(gca,'FontSize',12)
